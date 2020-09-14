53fa4c21b22d5a2573da29e38990a3bb1b53a88d
Your Name <you@example.com>
2020-09-13 22:56:30 -0300

    orm_api finalizado

---
+++ Domains/Produto.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Senai.EfCore.Tarde.Domains
+{
+    /// <summary>
+    /// Define a classe produto
+    /// </summary>
+    public class Produto : BaseDomain
+    {
+        public string Nome { get; set; }
+        public float Preco { get; set; }
+
+        //Relacionamento com a tabela PedidoItem 1,N
+        public List<PedidoItem> PedidosItens { get; set; }
+    }
+}
